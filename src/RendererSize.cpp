e04caeeae6cbb088227a6df88b5e016e168ed90d
BrunoGSA <brunogsa@live.com>
2020-03-01 12:57:15 -0300

    Build improvements

---
+++ src/RendererSize.cpp
@@ -0,0 +1,13 @@
+#include "RendererSize.h"
+
+static RendererSize* instance;
+
+void RendererSize::init()
+{
+	instance = ALLOC_NEW(RendererSize);
+}
+
+RendererSize* RendererSize::getInstance()
+{
+	return instance;
+}
