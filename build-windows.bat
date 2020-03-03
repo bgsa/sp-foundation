e1853c56f3bbfa6bb1e28e4bfba7904e47c066be
BrunoGSA <brunogsa@live.com>
2020-03-03 00:29:12 -0300

    Script build for windows and linux

---
+++ build-windows.bat
@@ -0,0 +1,24 @@
+@echo off 
+
+SET BUILD_DIR=build
+SET BUILD_TYPE=Release
+SET SHARED_LIB=OFF
+
+if exist %BUILD_DIR% ( rmdir /s/q %BUILD_DIR% )
+mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+
+cmake .. -G "Visual Studio 15 2017 Win64"     ^
+	-DOPERATING_SYSTEM:STRING=WINDOWS         ^
+	-DARCH_BITS:STRING=64                     ^
+	-DBUILD_SHARED_LIBS:BOOL=%SHARED_LIB%     ^
+	-DCMAKE_BUILD_TYPE:STRING=%BUILD_TYPE%    ^
+	-DCMAKE_ENABLE_EXPORTS:BOOL=ON            ^
+	-DUSE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=ON    ^
+	-DGLFW_ENABLED:BOOL=ON                    ^
+	-DQT_ENABLED:BOOL=OFF
+
+cmake --build . --config %BUILD_TYPE%
+
+cd ..\
+if exist %BUILD_DIR% ( rmdir /s/q %BUILD_DIR% )
