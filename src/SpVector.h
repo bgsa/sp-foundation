62a1de23d1849783135c92a1969230508dee0270
BrunoGSA <brunogsa@live.com>
2020-05-02 17:15:58 -0300

    SpVector added

---
+++ src/SpVector.h
@@ -0,0 +1,181 @@
+#ifndef SP_VECTOR_HEADER
+#define SP_VECTOR_HEADER
+
+#include "SpectrumFoundation.h"
+
+namespace NAMESPACE_FOUNDATION
+{
+	template <typename T>
+	class SpVectorItem
+		: public Object
+	{
+		template <typename R> friend class SpVector;
+	protected:
+		SpVectorItem<T>* _next;
+		T _data;
+
+		API_INTERFACE SpVectorItem<T>* addNext(const T& data)
+		{
+			_next = sp_mem_new(SpVectorItem<T>)(data);
+			_next->_data = data;
+
+			return _next;
+		}
+
+	public:
+		
+		API_INTERFACE SpVectorItem()
+		{
+			_next = NULL;
+			_data = NULL;
+		}
+
+		API_INTERFACE SpVectorItem(const T& data)
+		{
+			_next = NULL;
+			_data = data;
+		}
+
+		API_INTERFACE inline T value()
+		{
+			return _data;
+		}
+
+		API_INTERFACE inline SpVectorItem<T>* next()
+		{
+			return _next;
+		}
+
+		API_INTERFACE inline virtual const sp_char* toString() override
+		{
+			return "SpVectorItem";
+		}
+
+		API_INTERFACE inline virtual void dispose() override
+		{
+			if (_next != NULL)
+			{
+				sp_mem_delete(_next, SpVectorItem<T>);
+				_next = NULL;
+			}
+		}
+
+		API_INTERFACE ~SpVectorItem()
+		{
+			dispose();
+		}
+
+	};
+
+	template <typename T>
+	class SpVector
+		: public Object
+	{
+	protected:
+		sp_uint _length;
+		SpVectorItem<T>* _first;
+		SpVectorItem<T>* _last;
+
+	public:
+
+		API_INTERFACE SpVector() 
+		{
+			_length = ZERO_UINT;
+			_first = NULL;
+		}
+		
+		API_INTERFACE inline sp_uint length() noexcept
+		{
+			return _length;
+		}
+
+		API_INTERFACE inline sp_bool isEmpty() noexcept
+		{
+			return _length == ZERO_UINT;
+		}
+
+		API_INTERFACE inline SpVectorItem<T>* begin() const noexcept
+		{
+			return _first;
+		}
+
+		API_INTERFACE inline SpVectorItem<T>* last() const noexcept
+		{
+			return _last;
+		}
+
+		API_INTERFACE inline virtual SpVectorItem<T>* add(const T& value)
+		{
+			if (_first == NULL)
+			{
+				_first = sp_mem_new(SpVectorItem<T>)(value);
+				_last = _first;
+			}
+			else
+			{
+				_last = _last->addNext(value);
+			}
+
+			_length++;
+			return _last;
+		}
+
+		API_INTERFACE inline T operator[](const sp_int index) const
+		{
+			assert(index >= ZERO_INT && (sp_uint)index < _length);
+
+			SpVectorItem<T>* item = _first;
+			sp_int counter = ZERO_INT;
+
+			while (item != NULL && index != counter)
+			{
+				item = item->next();
+				counter++;
+			}
+
+			return item->_data;
+		}
+
+		API_INTERFACE inline T operator[](const sp_uint index) const
+		{
+			assert(index >= ZERO_UINT && index < _length);
+			return _data[index];
+		}
+
+#ifdef ENV_64BITS
+		API_INTERFACE inline T operator[](const sp_size index) const
+		{
+			assert(index >= ZERO_SIZE && (sp_uint)index < _length);
+			return _data[index];
+		}
+#endif
+
+		API_INTERFACE inline virtual const sp_char* toString() override
+		{
+			return "Spvector";
+		}
+
+		API_INTERFACE inline virtual void dispose() override
+		{
+			if (_first != NULL)
+			{
+				sp_mem_delete(_first, SpVectorItem<T>);
+
+				_first = NULL;
+				_last = NULL;
+				_length = ZERO_UINT;
+			}
+		}
+
+		API_INTERFACE ~SpVector()
+		{
+			dispose();
+		}
+
+	};
+
+}
+
+#include "SpVectorOfReferences.h"
+
+#endif // SP_VECTOR_HEADER
---
+++ test/src/SpVectorTest.cpp
@@ -0,0 +1,65 @@
+#include "SpectrumFoundationTest.h"
+#include "SpVector.h"
+
+#define CLASS_NAME SpVectorTest
+
+namespace NAMESPACE_FOUNDATION_TEST
+{
+	SP_TEST_CLASS(CLASS_NAME)
+	{
+	public:
+		SP_TEST_METHOD_DEF(add);
+		SP_TEST_METHOD_DEF(length);
+		SP_TEST_METHOD_DEF(dispose);
+	};
+	
+	SP_TEST_METHOD(CLASS_NAME, add)
+	{
+		SpVector<sp_int> vec;
+		SpVectorItem<sp_int>* item1 = vec.add(TWO_INT);
+		SpVectorItem<sp_int>* item2 = vec.add(FOUR_INT);
+
+		Assert::AreEqual(TWO_INT, vec[0]);
+		Assert::AreEqual(FOUR_INT, vec[1]);
+
+		Assert::IsTrue(item1->next() == item2);
+	}
+
+	SP_TEST_METHOD(CLASS_NAME, length)
+	{
+		SpVector<sp_int> vec;
+		vec.add(TWO_INT);
+		vec.add(FOUR_INT);
+
+		Assert::AreEqual(TWO_UINT, vec.length());
+	}
+
+	SP_TEST_METHOD(CLASS_NAME, dispose)
+	{
+		SpVector<sp_int> vec1;
+		SpVectorItem<sp_int>* item1 = vec1.add(TWO_INT);
+		SpVectorItem<sp_int>* item2  = vec1.add(FOUR_INT);
+
+		vec1.dispose();
+
+
+		sp_int* arrInt1 = (sp_int*)sp_mem_calloc(2u, SIZEOF_INT);
+		sp_int* arrInt2 = (sp_int*)sp_mem_calloc(2u, SIZEOF_INT);
+		arrInt1[0] = 1;
+		arrInt1[1] = 2;
+		arrInt2[0] = 3;
+		arrInt2[1] = 4;
+
+		SpVector<sp_int*> vec2;
+		vec2.add(arrInt1);
+		vec2.add(arrInt2);
+
+		Assert::AreEqual(1, vec2[0][0]);
+		Assert::AreEqual(2, vec2[0][1]);
+		Assert::AreEqual(3, vec2[1][0]);
+		Assert::AreEqual(4, vec2[1][1]);
+
+		vec2.dispose();
+	}
+
+}
