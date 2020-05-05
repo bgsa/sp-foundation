9277151a9d261784eb03b9700ce9587ff23f9acc
BrunoGSA <brunogsa@live.com>
2020-05-05 00:17:33 -0300

    Small fixes

--- src/ImageBMP.h
+++ src/ImageBMP.h
@@ -34,6 +34,15 @@ typedef struct
 	sp_uint   colorsUsed;      /* Number of colors used */
 	sp_uint   colorsImportant; /* Number of important colors */
 } BMPHeaderInfo;
+typedef struct 
+{
+	sp_uint red_mask{ 0x00ff0000 };         // Bit mask for the red channel
+	sp_uint green_mask{ 0x0000ff00 };       // Bit mask for the green channel
+	sp_uint blue_mask{ 0x000000ff };        // Bit mask for the blue channel
+	sp_uint alpha_mask{ 0xff000000 };       // Bit mask for the alpha channel
+	sp_int color_space_type{ 0x73524742 }; // Default "sRGB" (0x73524742)
+	sp_int unused[16]{ 0 };                // Unused data for sRGB color space
+} BMPColorHeader;
 #pragma pack(pop)
 
 namespace NAMESPACE_FOUNDATION
@@ -41,17 +50,26 @@ namespace NAMESPACE_FOUNDATION
 	class ImageBMP 
 		: public Image
 	{
+	private:
+
+		inline void convertFromBGRToRGB(const sp_size size)
+		{
+			for (sp_size i = ZERO_SIZE; i < size; i += THREE_SIZE)
+				std::swap(_data[i], _data[i + TWO_SIZE]);
+		}
+
 	public:
+
+		ImageBMP()
+		{
+			colorFormat = 0x1907; // TODO: REMOVE!  GL_RGB constant
+		}
 		
 		API_INTERFACE inline ColorRGB getPixelRGB(sp_int x, sp_int y) override;
 		
 		API_INTERFACE static ImageBMP* load(const sp_char* filename)
 		{
-			const sp_uint opengl_RGB = 0x1907; //TODO: REMOVE!
-
 			ImageBMP *image = sp_mem_new(ImageBMP)();
-			image->colorFormat = opengl_RGB;
-
 			BMPFileHeader fileHeader;
 			BMPHeaderInfo headerInfo;
 					
@@ -79,47 +97,20 @@ namespace NAMESPACE_FOUNDATION
 			fread(&fileHeader, sizeof(BMPFileHeader), 1, file);
 			fread(&headerInfo, sizeof(BMPHeaderInfo), 1, file);
 
+			//sp_assert(headerInfo.bitCount == 8); // BMP images should be 8 bits ?!
+
 			image->_width = headerInfo.width;
 			image->_height = headerInfo.height;
 
-			fseek(file, 54 * SIZEOF_CHAR, SEEK_SET); //move o cursor para a posi��o 54 do arquivo, contando do in�cio
+			fseek(file, fileHeader.offBits, SEEK_SET);
 			
-			const sp_uint size = 3 * image->_width * image->_height;
-			image->_data = (sp_uchar*) sp_mem_calloc(size, SIZEOF_UCHAR); // allocate 3 bytes per pixel
-			fread(image->_data, SIZEOF_UCHAR, size, file); // read the rest of the data at once
+			const sp_size size = 3 * image->_width * image->_height;
+			image->_data = (sp_uchar*) sp_mem_calloc(size, SIZEOF_UCHAR);
+			fread(image->_data, SIZEOF_UCHAR, size, file);
 
 			fclose(file);
 	#endif
-			//coloca no formato RGB ao inv�s de BGR
-			/*		
-			for (int i = 0; i < size; i += 3)		
-			{
-				unsigned char tmp = image->data[i];
-				image->data[i] = image->data[i + 2];
-				image->data[i + 2] = tmp;
-			}
-			*/
-
-			/*
-			int padding = 0;
-			int scanlinebytes = image->width * 3;
-			while ((scanlinebytes + padding) % 4 != 0)
-				padding++;
-			int psw = scanlinebytes + padding;
-			
-			long bufpos = 0;
-			long newpos = 0;
-			for (int y = 0; y < image->height; y++)
-				for (int x = 0; x < 3 * image->width; x += 3)
-				{
-					newpos = y * 3 * image->width + x;
-					bufpos = (image->height - y - 1) * psw + x;
-
-					image->data[newpos] = image->data[bufpos + 2];
-					image->data[newpos + 1] = image->data[bufpos + 1];
-					image->data[newpos + 2] = image->data[bufpos];
-				}
-				*/
+			image->convertFromBGRToRGB(size);
 
 			return image;
 		}
