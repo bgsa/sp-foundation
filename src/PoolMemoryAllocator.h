e17e0496dfeaa0d0e59797b2ae1966bdef5f6355
BrunoGSA <brunogsa@live.com>
2020-05-14 23:52:21 -0300

    sp_mem_new_array function added

--- src/PoolMemoryAllocator.h
+++ src/PoolMemoryAllocator.h
@@ -31,6 +31,7 @@
 
 #ifdef __cplusplus
 	#define sp_mem_new(Type) new (NAMESPACE_FOUNDATION::PoolMemoryAllocator::main()->alloc(sizeof(Type), ++NAMESPACE_FOUNDATION::PoolMemoryAllocator::main()->syncPreviousCounter)) Type
+	#define sp_mem_new_array(Type, length) new (NAMESPACE_FOUNDATION::PoolMemoryAllocator::main()->alloc(sizeof(Type) * length, ++NAMESPACE_FOUNDATION::PoolMemoryAllocator::main()->syncPreviousCounter)) Type[ length ]
 	#define sp_mem_new_templated1(Type, Type1) new (NAMESPACE_FOUNDATION::PoolMemoryAllocator::main()->alloc(sizeof(Type), ++NAMESPACE_FOUNDATION::PoolMemoryAllocator::main()->syncPreviousCounter)) Type<Type1>
 	#define sp_mem_new_templated2(Type, Type1, Type2) new (NAMESPACE_FOUNDATION::PoolMemoryAllocator::main()->alloc(sizeof(Type<Type1,Type2>), ++NAMESPACE_FOUNDATION::PoolMemoryAllocator::main()->syncPreviousCounter)) Type<Type1, Type2>
 	#define sp_mem_delete(buffer, Type) (buffer)->~Type(); sp_mem_release(buffer);
